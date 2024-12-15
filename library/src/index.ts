bd82fdb43772a4fad715f8b02d265232e7693b18
Yash Singh <dobbyneedsclothes@gmail.com>
2024-12-15 22:47:00 +0530

    server mongodb

--- library/src/index.ts
+++ library/src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect("mongodb://localhost:27017/library", {
+mongoose.connect("mongodb://127.0.0.1:27017/library", {
   // Removed deprecated options
 });
 
